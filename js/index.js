9e97050929d58ca9b2efad16d1de493fc02d264e
rijanpaudel <paudelrijan369@gmail.com>
2024-08-27 20:18:59 +0545

    New Commit

--- js/index.js
+++ js/index.js
@@ -9,3 +9,19 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+window.addEventListener('scroll', function() {
+  var envatoNavbar = document.querySelector('.envato');
+  var mainNav = document.querySelector('.main-nav');
+  var searchNav = document.querySelector('.search-nav');
+  
+  // Get the distance from the top of the page to the search-nav
+  var searchNavBottom = searchNav.getBoundingClientRect().bottom;
+
+  // If search-nav is out of view, fix main-nav
+  if (searchNavBottom <= 0) {
+      mainNav.classList.add('fixed-nav');
+  } else {
+      mainNav.classList.remove('fixed-nav');
+  }
+});
